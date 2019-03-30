d916462c1511c496f6eaef6fb6e5384630f13de5
barakyosi <barakyosi@gmail.com>
2019-03-30 10:35:39 +0300

    Adding ui

---
+++ ui/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Map from './Map'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
